b77045fd97550a7a90a2f4b4d26ebf5a94411d9c
Madina <madina-khat@yandex.ru>
2022-12-07 20:32:20 +0300

    Задачи 1, 2, 3, 4

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,16 @@ public class Main {
 
         System.out.println(maksim + "\n" + anya + "\n" + katya + "\n" + artem);
 
-    Car lada = new Car("Lada", "Granta", 2015, "Россия", "жёлтый",
-            1.7f, "МКПП", "Седан", "а111АА111", 5, true);
-    Car audi = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный",
-            3.0f,"АКПП", "Хетчбек", "В222ВВ222", 5, true);
-    Car bmw = new Car("BMW", "Z8", 2021, "Германия", "черный",
-            -2,"АКПП", "Купе", "Е333ЕЕ123", 2, true);
-    Car kia = new Car("Kia", "Sportage 4 поколения", 2018, "Южная Корея", "красный",
-            0,"АКПП", "Кроссовер", "М444ММ147", 7, false);
+        Car lada = new Car("Lada", "Granta", 2015, "Россия", "жёлтый",
+                1.7f, "МКПП", "Седан", "а111АА111", 5, true);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный",
+                3.0f,"АКПП", "Хетчбек", "В222ВВ222", 5, true);
+        Car bmw = new Car("BMW", "Z8", 2021, "Германия", "черный",
+                -2,"АКПП", "Купе", "Е333ЕЕ123", 2, true);
+        Car kia = new Car("Kia", "Sportage 4 поколения", 2018, "Южная Корея", "красный",
+                0,"АКПП", "Кроссовер", "М444ММ147", 7, false);
 
         System.out.println(lada + "\n" + audi + "\n" + bmw + "\n" + kia);
-}}
+
+}
+}
